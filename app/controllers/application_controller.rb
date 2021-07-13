7c55f9231d5cc434595ff41c06911f013a52771e
github username <github email address>
2021-07-13 15:46:26 -0400

    Add a couple methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < Sinatra::Base
   end
 
   options "*" do
-    response.headers["Allow"] = "HEAD,GET,PUT,POST,DELETE,OPTIONS"
+    response.headers["Allow"] = "HEAD,GET,PUT,PATCH,POST,DELETE,OPTIONS"
     response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, X-HTTP-Method-Override, Content-Type, Cache-Control, Accept"
     200
   end
@@ -17,5 +17,21 @@ class ApplicationController < Sinatra::Base
   # method "URL" do
     
   # end
+  get "/movies" do
+    Movie.all.to_json
+  end
+
+  get "/users" do
+    User.all.to_json
+  end
+
+  get "/users/:id" do
+    user = User.find(params[:id])
+    user.to_json
+  end
+
+  get "/users/:id/groups" do
+    User.find(params[:id]).groups.to_json
+  end
 
 end
