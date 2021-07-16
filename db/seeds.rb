bbe02497158d73341b4fdf2fe50ede13481c3c3a
github username <github email address>
2021-07-15 21:07:07 -0400

    reseed some stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,27 +12,12 @@ UserGroup.reset_pk_sequence
 User.reset_pk_sequence
 Vote.reset_pk_sequence
 
-Group.create(group_name: "group 1")
-Group.create(group_name: "group 2")
+Group.create(group_name: "FlatMates Movie Crew")
+Group.create(group_name: "Something Else")
 
-User.create(name: "Kyle")
-User.create(name: "Nisa")
-User.create(name: "Greg")
-User.create(name: "Arthur")
-User.create(name: "Lucy")
-User.create(name: "Adrienne")
-User.create(name: "Carlos")
-User.create(name: "Edward")
-User.create(name: "Ryan")
-User.create(name: "Brendan")
-User.create(name: "Kenny")
-User.create(name: "Terry")
-User.create(name: "Shevon")
-User.create(name: "Adreena")
-User.create(name: "Zeus")
-User.create(name: "Kenny")
-User.create(name: "Darren")
-User.create(name: "Jack")
+users = ["Kyle", "Nisa", "Greg", "Arthur", "Lucy", "Adrienne", "Carlos", "Edward", "Ryan", "Brendan", "Kenny", "Terry", "Shevon", "Adreena", "Zeus", "Darren", "Jack", "Dan"]
+users.each {|user| User.create(name: user)}
+
 
 Movie.create(title: "Spirited Away", genre: "Fantasy", rating: 8.6, release_date: 2001, image: "https://m.media-amazon.com/images/M/MV5BMjlmZmI5MDctNDE2YS00YWE0LWE5ZWItZDBhYWQ0NTcxNWRhXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg")
 Movie.create(title: "Harry Potter and the Prisoner of Azkaban", genre: "Adventure", rating: 7.9, release_date: 2004, image: "https://m.media-amazon.com/images/M/MV5BMTY4NTIwODg0N15BMl5BanBnXkFtZTcwOTc0MjEzMw@@._V1_SX300.jpg")
@@ -43,7 +28,7 @@ Movie.create(title: "Minari", genre: "Drama", rating: 7.5, release_date: 2020, i
 Movie.create(title: "A Quiet Place Part II", genre: "Sci-Fi", rating: 7.5, release_date: 2020, image: "https://m.media-amazon.com/images/M/MV5BMTE2ODU4NDEtNmRjNS00OTk1LTg4NmMtNTAzYzVlNzJmYjgzXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg")
 Movie.create(title: "In the Heights", genre: "Musical", rating: 7.7, release_date: 2021, image: "https://m.media-amazon.com/images/M/MV5BMzM1ZDY0YTktZTYzZi00MjZjLTllMDMtMmNlMmM5NmY4ZjllXkEyXkFqcGdeQXVyMTA1OTcyNDQ4._V1_SX300.jpg")
 Movie.create(title: "The Room", genre: "Drama", rating: 3.7, release_date: 2003, image: "https://m.media-amazon.com/images/M/MV5BYjEzN2FlYmYtNDkwMC00NGFkLWE5ODctYmE5NmYxNzE2MmRiXkEyXkFqcGdeQXVyMjMwODc5Mw@@._V1_SX300.jpg")
-
+Movie.create(title: "Toy Story", genre: "Animation, Adventure, Comedy, Family, Fantasy", rating: 8.3, release_date: 1995, image: "https://m.media-amazon.com/images/M/MV5BMDU2ZWJlMjktMTRhMy00ZTA5LWEzNDgtYmNmZTEwZTViZWJkXkEyXkFqcGdeQXVyNDQ2OTk4MzI@._V1_SX300.jpg")
 
 GroupMovie.create(movie_id: 1, group_id: 1)
 GroupMovie.create(movie_id: 2, group_id: 1)
@@ -55,14 +40,10 @@ GroupMovie.create(movie_id: 7, group_id: 2)
 GroupMovie.create(movie_id: 8, group_id: 2)
 GroupMovie.create(movie_id: 9, group_id: 2)
 GroupMovie.create(movie_id: 4, group_id: 2)
+GroupMovie.create(movie_id: 10, group_id: 1)
 
 
-
-UserGroup.create(user_id: 1, group_id: 1)
-UserGroup.create(user_id: 2, group_id: 1)
-UserGroup.create(user_id: 3, group_id: 1)
-UserGroup.create(user_id: 4, group_id: 1)
-UserGroup.create(user_id: 5, group_id: 1)
+User.all.each {|user| UserGroup.create(user_id: user.id, group_id: 1)}
 
 UserGroup.create(user_id: 6, group_id: 2)
 UserGroup.create(user_id: 7, group_id: 2)
@@ -70,6 +51,8 @@ UserGroup.create(user_id: 8, group_id: 2)
 UserGroup.create(user_id: 9, group_id: 2)
 UserGroup.create(user_id: 10, group_id: 2)
 UserGroup.create(user_id: 4, group_id: 2)
+
+Group.first.update(winner: 10)
 
 # 3 way tie group 2
 # Vote.create(user_group_id: 6, movie_id: 1, rank: 1)
